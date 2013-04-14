85659f89546a065cf3dc512586cf8daf1b97a6b1
Joe Nelson <cred+github@begriffs.com>
2013-04-13 23:00:21 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lucre::Application.config.secret_token = '60cefa1d49c5e103136d2cbd3805aedf73211bf38fd29b833d3554c065f443fc3d016ffb89b7a9b65b11098cac9ab3ccadb139eadd48214388a8e923d6130c66'
